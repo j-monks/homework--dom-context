67072b46bb0a3b5cb7bd8e28d9dcae2703c5d430
James Monks <J.monks23@gmail.com>
2020-07-24 11:47:02 +0100

    initial commit

---
+++ hw_dom/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JavaScript loaded");
+
+    const form = document.querySelector("#new-item-form");
+    form.addEventListener("submit", handleFormSubmit);
+
+    const deleteAllButton = document.querySelector("#delete-all");
+    deleteAllButton.addEventListener("click", handleDeleteClick);
+});
